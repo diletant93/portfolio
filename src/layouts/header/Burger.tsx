2036ff1af2229148d7234ccab8884613f95f7990
Artur <artur.talimonchyk@gmail.com>
2025-02-04 18:37:33 +0100

    burger: styles, no functionality yet

--- src/layouts/header/Burger.tsx
+++ src/layouts/header/Burger.tsx
@@ -1,7 +1,13 @@
 import Button from "@/components/ui/Button";
+import { cn } from "@/utils/cn";
 
 export default function Burger() {
+    const lineStyles = "block w-5 bg-secondary h-[2px]"
     return (
-        <Button>Hello</Button>
+        <Button className="border-0 p-0 hover:bg-transparent hover:scale-x-150 hover:scale-y-125 transition-all origin-right">
+            <span className={lineStyles}></span>
+            <span className={cn(lineStyles, 'w-3 self-end my-2 ml-auto')}></span>
+            <span className={lineStyles}></span>
+        </Button>
     );
 }
