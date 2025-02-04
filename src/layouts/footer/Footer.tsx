0b264356e5b852934877efa3ef5cf2bd415e1731
Artur <artur.talimonchyk@gmail.com>
2025-02-04 16:06:09 +0100

    footer content , no styles nor functionality

---
+++ src/layouts/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import Copyright from "@/components/Copyright";
+import Socials from "@/components/Socials";
+
+export default function Footer() {
+  return (
+    <div>
+        <Socials/>
+       <p>Made with love and code</p>
+       <Copyright/>
+    </div>
+  );
+}
