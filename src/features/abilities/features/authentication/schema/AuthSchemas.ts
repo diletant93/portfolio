c57d35fad185641ba28db5fd5477f4c592ff6d3d
Artur <artur.talimonchyk@gmail.com>
2025-02-21 09:52:57 +0100

    logging in

--- src/features/abilities/features/authentication/schema/AuthSchemas.ts
+++ src/features/abilities/features/authentication/schema/AuthSchemas.ts
@@ -3,17 +3,6 @@ const base = {
   email: z
     .string({ required_error: "Email is necessary" })
     .email({ message: "Invalid email format" }),
-
-  password: z
-    .string({ required_error: "Password is necessary" })
-    .min(6, { message: "Password must be at least 6 characters long" })
-    .max(50, { message: "Password must be at most 50 characters long" })
-    .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .regex(/[0-9]/, { message: "Password must contain at least one number" }),
-
-  confirmPassword: z.string({ required_error: "Please confirm your password" }),
 };
 
 export const registerValidationSchema = z
@@ -26,18 +15,25 @@ export const registerValidationSchema = z
       .regex(/^[a-zA-Z\s]+$/, {
         message: "Name can only contain letters and spaces",
       }),
-      
+    password: z
+      .string({ required_error: "Password is necessary" })
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .max(50, { message: "Password must be at most 50 characters long" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[0-9]/, { message: "Password must contain at least one number" }),
+
+    confirmPassword: z.string({
+      required_error: "Please confirm your password",
+    }),
   })
   .refine(({ password, confirmPassword }) => password === confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
 
-export const loginValidationSchema = z
-  .object({
-    ...base,
-  })
-  .refine(({ password, confirmPassword }) => password === confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
+export const loginValidationSchema = z.object({
+  ...base,
+  password: z.string({ required_error: "Password is necessary" }),
+});
