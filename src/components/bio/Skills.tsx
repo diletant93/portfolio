792125a37411fce4f44a1fb176be8e23c83956ff
Artur <artur.talimonchyk@gmail.com>
2025-03-02 19:05:30 +0100

    technical skills content

--- src/components/bio/Skills.tsx
+++ src/components/bio/Skills.tsx
@@ -1,15 +1,33 @@
 import BioHeading from "./BioHeading";
-
-function SkillsItem(){
-
+type SkillItem = {
+    name:string
+}
+const skillsList:SkillItem[] = [
+    {
+        name:'C#'
+    },
+    {
+        name:'JS'
+    },
+    {
+        name:'TS'
+    }
+]
+function SkillElement({skill}:{skill:SkillItem}){
+    return <li className="inline-block p-5 bg-secondary text-primary rounded-sm">
+        {skill.name}
+    </li>
 }
 function SkillsList(){
-
+    return <ul className="flex flex-col gap-3">
+        {skillsList.map(element => <SkillElement key={element.name} skill={element}/>)}
+    </ul>
 }
 export default function Skills() {
   return (
     <div>
        <BioHeading>Technical skills</BioHeading>
+       <SkillsList/>
     </div>
   );
 }
