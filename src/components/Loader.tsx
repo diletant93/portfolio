12cac63b7e9520c108339092478b9b20f0c6991e
Artur <artur.talimonchyk@gmail.com>
2025-03-14 18:11:06 +0100

    enhanced the uploader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,13 @@
+import {Triangle} from 'react-loader-spinner'
 export default function Loader() {
   return (
-    <div className="absolute top-[45%] left-1/2 -translate-x-1/2 w-7 h-7">
-       <img src="icons/loader.svg" alt="" className="h-full w-full bg-transparent"/>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+     <Triangle
+     visible={true}
+     height={40}
+     width={40}
+     color='#fff'
+     ariaLabel='triangle-loading'/>
     </div>
   );
 }
