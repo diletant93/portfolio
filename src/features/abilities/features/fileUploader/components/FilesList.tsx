7eb1c997025c5e774a8f9d0800ce76fc4239a954
Artur <artur.talimonchyk@gmail.com>
2025-03-14 18:19:53 +0100

    fixed all the loader positions

--- src/features/abilities/features/fileUploader/components/FilesList.tsx
+++ src/features/abilities/features/fileUploader/components/FilesList.tsx
@@ -3,12 +3,18 @@ import { useFiles } from "../hooks/useFiles";
 import FileListItem from "./FileListItem";
 
 export default function FilesList() {
-  const {files, isLoading} = useFiles()
-  if(isLoading || !files) return <Loader/>
-  if(files?.length === 0) return <div className="w-full h-full flex-center uppercase">There's no files yet</div>
+  const { files, isLoading } = useFiles()
+  if (files?.length === 0) return <div className="w-full h-full flex-center uppercase">There's no files yet</div>
   return (
-    <div className="pr-2 space-y-2">
-      {files.map(file => <FileListItem name={file.name} />)}
+    <div className="pr-2 space-y-2 h-full w-full relative">
+      {isLoading || !files ? (<Loader />) : (
+        <>
+          {
+            files.map(file => <FileListItem name={file.name} />)
+          }
+        </>
+      )}
+
     </div>
   );
 }
