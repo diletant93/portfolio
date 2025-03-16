33c9542281572d33bd2d682f82e2075cb779fa43
Artur <artur.talimonchyk@gmail.com>
2025-03-16 20:02:24 +0100

    fixed some paddings fo the bio for the md and smalelr

--- src/pages/Bio.tsx
+++ src/pages/Bio.tsx
@@ -7,7 +7,7 @@ export default function Bio() {
   return (
     <section className="h-full flex flex-col">
       <PageTitle>My Journey</PageTitle>
-      <div className="grid grid-cols-1 md:grid-cols-2 auto-row-[minmax(auto,50%)]  p-10 gap-4 justify-items-center flex-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 auto-row-[minmax(auto,50%)] px-5 pb-5 md:p-10  justify-items-center flex-1">
         <Education />
         <Skills />
         <PersonalProjects className="md:col-span-2 w-full lg:w-2/3 text-center space-y-6" />
