2af45bccdc73d843f4ec6a681296901ea802d594
Artur <artur.talimonchyk@gmail.com>
2025-03-30 17:40:12 +0200

    added two more projects to the projects list on the bio page

--- src/constants/projectItems.ts
+++ src/constants/projectItems.ts
@@ -1,14 +1,23 @@
-
 export type ProjectEntity = {
   imgUrl: string;
   title: string;
-  projectUrl:string;
+  projectUrl: string;
 };
 
 export const projectsArray: ProjectEntity[] = [
   {
-    imgUrl: 'storeit_photo.jpg',
-    title: 'StoreIt',
-    projectUrl:'https://store-it-sable-psi.vercel.app/'
+    imgUrl: "storeit_photo.jpg",
+    title: "StoreIt",
+    projectUrl: "https://store-it-sable-psi.vercel.app/",
+  },
+  {
+    imgUrl: "wild_oasis_dashboard.png",
+    title: "The Wild Oasis / Dashboard",
+    projectUrl: "https://the-wild-oasis-wheat-three.vercel.app/",
+  },
+  {
+    imgUrl: "wild_oasis.png",
+    title: "The Wild Oasis",
+    projectUrl: "https://wildoasis-nine.vercel.app/",
   },
 ];
