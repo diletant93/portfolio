0d8dd93101f9d147fdccc9684c938d75a5ba87f6
Artur <artur.talimonchyk@gmail.com>
2025-04-09 20:56:40 +0200

    changed a bit the paddings of the big socials on the home page

--- src/components/BigSocials.tsx
+++ src/components/BigSocials.tsx
@@ -2,7 +2,7 @@
 import Socials from "./Socials";
 export default function BigSocials() {
   return (
-    <div className="flex justify-center gap-3 py-7  md:py-20 items-center px-2">
+    <div className="flex justify-center gap-3 py-7  md:py-10 items-center px-2">
       <p className="uppercase text-xl">Here are my socials:</p> <Socials />
     </div>
   );
